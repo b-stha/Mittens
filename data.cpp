1a099dd5695ed64a1211e42d898942f7eee2226e
b-stha <biprasstha@gmail.com>
2024-06-12 23:44:00 -0700

    implemented unit emoji id map for most recent match

---
+++ data.cpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+#include <string>
+
+
+std::unordered_map<std::string, std::vector<std::string>> unitData = {
+	{"TFT11_Bard", {"Bard", "<:tft11_bard:1250562448340156466>"}},
+	{"TFT11_Sivir", {"Sivir", "<:tft11_sivir:1250562511447785614>"}},
+	{"TFT11_Garen", {"Garen", "<:tft11_garen:1250562458720927786>"}},
+	{"TFT11_Zyra", {"Zyra", "<:tft11_zyra:1250691821089062922>"}},
+	{"TFT11_Riven", {"Riven", "<:tft11_riven:1250562504392704091>"}},
+	{"TFT11_Diana", {"Diana", "<:tft11_diana:1250562454992326757>"}},
+	{"TFT11_Zoe", {"Zoe", "<:tft11_zoe:1250696806942249022>"}},
+	{"TFT11_LeeSin", {"Lee Sin", "<:tft11_leesin:1250562483513724988>"}},
+	{"TFT11_Galio", {"Galio", "<:tft11_galio:1250562836510408736>"}},
+	{"TFT11_Kayle", {"Kayle", "<:tft11_kayle:1250562472969113722> "}},
+};
