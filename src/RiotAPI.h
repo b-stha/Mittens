bc1fe659c630aadec12a42516d4e07e0a65a6911
Bipras Shrestha <biprasstha@gmail.com>
2025-11-28 23:51:59 -0800

    fixed riotapi headers

--- src/RiotAPI.h
+++ src/RiotAPI.h
@@ -1,8 +1,8 @@
 #ifndef RIOTAPI_H
 #define RIOTAPI_H
 
-#include "parsejson.h"
-#include "helpers.h"
+#include <string>
+#include "Player.h"
 
 std::string fetchMatchID(const Player& player, const std::string apiKey);
 Info fetchInfo(const std::string matchID, const std::string apiKey);
