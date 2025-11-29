ba9ae21412c597d10fb93c4ca299d22824c6f9c2
Bipras Shrestha <biprasstha@gmail.com>
2025-11-28 23:53:33 -0800

    moved discord bot loading to Bot class

--- src/main.cpp
+++ src/main.cpp
@@ -10,95 +10,14 @@
 
 using json = nlohmann::json;
 
-std::atomic <bool> running = false;
-std::vector<std::unique_ptr<Player>> userVec;
-
-void stop() {
-    running = false;
-}
-
 int main() {
-    auto loadedData = loadJson();
-    dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
-
-    bot.on_log(dpp::utility::cout_logger());
-
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        }
-    });
-
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
-
-            dpp::slashcommand add;
-            add.set_name("add");
-            add.set_description("Format as name#tagline.");
-            add.add_option(
-                dpp::command_option(dpp::co_string, "username", "name#tag", true)
-            );
-
-            bot.global_command_create(add, [&](const dpp::confirmation_callback_t& callback) {
-                if (callback.is_error()) {
-                    std::cout << callback.http_info.body << "\n";
-                }
-                });
-        }
-    });
-
-    bot.on_slashcommand([&bot](const dpp::interaction_create_t& event) {
-        if (event.command.type == dpp::it_application_command) {
-            dpp::command_interaction cmd_data = std::get<dpp::command_interaction>(event.command.data);
-            dpp::snowflake currChannel = event.command.channel_id;
-
-            if (cmd_data.name == "add") {
-                std::string userInput = std::get<std::string>(cmd_data.options[0].value);
-                std::cout << userInput << std::endl;
-                std::vector<std::string> userInputArr;
-
-                if (userInput.find("#") != std::string::npos) { 
-                    userInputArr = split(userInput, '#');
-                }
-                else {
-                    userInputArr.emplace_back(userInput);
-                    userInputArr.emplace_back("NA1");
-                }
-                
-                std::string puuid;
-
-                bool puuidFetchSuccess = false;
-
-                try {
-                    puuid = fetchPUUID(userInputArr[0], userInputArr[1], TFT_APIKEY);
-                    puuidFetchSuccess = true;
-                    event.reply(userInput + " successfully added");
-                }
-                catch (const std::exception& e) {
-                    std::cout << e.what() << std::endl; 
-                    event.reply(userInput + " not found...");
-                }
-
-                if (puuidFetchSuccess && notPlayerExists(userVec, puuid)) {
-                    std::unique_ptr pPlayer = std::make_unique<Player>(puuid);
-                    pPlayer->setChannelID(currChannel);
-                    pPlayer->setNameTag(userInputArr[0], userInputArr[1]);
-
-                    std::string summonerID = fetchSummonerID(puuid, TFT_APIKEY);
-                    pPlayer->setSummonerID(summonerID);
-                    League initLeague = fetchLeague(*pPlayer, TFT_APIKEY);
-                    pPlayer->setPlayerRank(initLeague);
-                    userVec.push_back(std::move(pPlayer));
-                }
-                else {
-                    event.reply(userInput + " already exists.");
-                }
-            }
-        }
-    });
-
-    bot.start(dpp::st_return);
+    try {
+        std::unique_ptr<Data> loadedData = std::make_unique<Data>();
+    } catch (const std::exception& e) {
+        std::cerr << "Error loading data: " << e.what() << std::endl;
+        return 1;
+    }
+    
 
     signal(SIGINT, [](int code) {
         stop();
