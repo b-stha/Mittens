1c5e501a95b1959b094488c861f67afd506c1d19
Bipras Shrestha <biprasstha@gmail.com>
2025-12-03 23:12:14 -0800

    created RiotAPI class

--- src/RiotAPI.h
+++ src/RiotAPI.h
@@ -4,12 +4,21 @@
 #include <string>
 #include "Player.h"
 
-std::string fetchMatchID(const Player& player, const std::string apiKey);
-Info fetchInfo(const std::string matchID, const std::string apiKey);
-void setName(Player& player, const std::string apiKey);
-std::string fetchPUUID(const std::string& name, const std::string& tag, const std::string& apiKey);
-std::string fetchSummonerID(const std::string& puuid, const std::string& apiKey);
-League fetchLeague(Player& player, const std::string& apiKey);
+class RiotAPI {
+public:
+    RiotAPI(dpp::cluster* bot, const std::string& apiKey)
+        : botCluster(bot), apiKey(apiKey) {}
+    void fetchMatchID(const Player& player);
+    void fetchInfo(const Player& player);
+    void setName(Player& player);
+    void fetchPUUID(const std::string& name, const std::string& tag);
+    void fetchSummonerID(const Player& player);
+    void fetchLeague(Player& player);
+
+private:
+    dpp::cluster* botCluster;
+    std::string apiKey;
+};
 
 #endif
 
