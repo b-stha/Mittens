e49ac3b8472e6689c94d195a73f3cdfe23b7a671
Bipras Shrestha <biprasstha@gmail.com>
2025-12-06 21:54:55 -0800

    calcBoardValue() moved to MatchInfo, deleted addTrait()

--- src/Player.cpp
+++ src/Player.cpp
@@ -82,43 +82,6 @@ void Player::setPrevTier(std::string tier) {
 	prevTier = tier;
 }
 
-void Player::addTrait(const Trait& trait) {
-	myMatchInfo.traits.push_back(trait);
-}
-
-
-int PlayerMatchInfo::calcBoardValue() {
-	int boardValue = 0;
-	int unitCount;
-	for (auto unit : units) {
-		if (unit.tier > 1) {
-			unitCount = 3 * (unit.tier - 1);
-		}
-		else {
-			unitCount = 1;
-		}
-
-		switch (unit.rarity)
-		{
-		case 0:
-			boardValue += 1 * unitCount;
-			break;
-		case 1:
-			boardValue += 2 * unitCount;
-			break;
-		case 2:
-			boardValue += 3 * unitCount;
-			break;
-		case 4:
-			boardValue += 4 * unitCount;
-			break;
-		case 6:
-			boardValue += 5 * unitCount;
-			break;
-		case 8:
-			boardValue += 6 * unitCount;
-			break;
-		}
-	}
-	return boardValue;
+const MatchInfo& Player::getMatchInfo() const {
+	return matchInfo;
 }
