4c53a9f78b63b9eedcc4569c6889f20b4db262c2
Bipras Shrestha <biprasstha@gmail.com>
2025-12-07 17:01:43 -0800

    moved to include

--- include/helpers.h
+++ include/helpers.h
@@ -1,9 +1,11 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-#include "Player.h"
-#include "data.h"
 #include <string>
+#include <vector>
+#include <memory>
+
+class Player;
 
 std::string setStrWidth(const std::string& str, int len);
 std::string operator * (std::string a, unsigned int b);
@@ -11,7 +13,5 @@ std::string starCount(const int& tier);
 std::vector<std::string> split(const std::string& s, char delim);
 bool notPlayerExists(const std::vector<std::unique_ptr<Player>>& players, const std::string& puuid);
 std::string fillSpaces(std::string input);
-int subtractLP(const Player& player);
-bool sortByStyle(const Trait& t1, const Trait& t2);
 
 #endif
